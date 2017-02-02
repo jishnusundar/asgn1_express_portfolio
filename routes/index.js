f387a5ce1b391b29e1b872c89c4ab57d52673589
= <jishnusundar423@gmail.com>
2017-02-01 21:55:04 -0500

    Made responsive

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  let currentDate = new Date();
+  res.render('index', { 
+    title: 'Express' ,
+    date: currentDate.toTimeString()
+  });
+});
+
+/* GET about page. */
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About' });
 });
 
 module.exports = router;
