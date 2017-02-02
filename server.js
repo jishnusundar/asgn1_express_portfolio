3dcb5c866840a22b8b96037073ef924f830a607f
= <jishnusundar423@gmail.com>
2017-02-01 19:20:31 -0500

    Simple express version

---
+++ server.js
@@ -0,0 +1,26 @@
+//your server will live here
+let express = require('express');
+
+//create an instance of the connect server - app
+let app = express();
+
+//create the port constant
+const port = 3000;
+
+//start listening
+app.listen(port);
+console.log(`Server started at http://localhost:${port}`);
+
+//Route for hello
+app.use('/hello', (req,res,next)=> {
+res.setHeader('Content-Type', 'text/plain');
+res.end("Hello world!");
+next();
+});
+
+//first route is '/'- root of my website
+app.use('/', (req,res,next)=> {
+res.setHeader('Content-Type', 'text/plain');
+res.end("Welcome!");
+next();
+});
