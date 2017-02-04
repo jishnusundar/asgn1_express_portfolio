132efa9d64241a6a50e41c06409b04e70a3922ad
= <jishnusundar423@gmail.com>
2017-02-04 17:04:49 -0500

    Added Routing for Each page

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,27 @@ router.get('/', (req, res, next) => {
   let currentDate = new Date();
   res.render('index', { 
     title: 'Hi there!' ,
-    date: currentDate.toTimeString()
   });
 });
 
-/* GET about page. */
+/* GET projects page. */
 router.get('/projects', (req, res, next) => {
   res.render('index', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', (req, res, next) => {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET about page. */
+router.get('/about', (req, res, next) => {
+  res.render('index', { title: 'Who\'s Me?' });
+});
+
+/* GET about page. */
+router.get('/contact', (req, res, next) => {
+  res.render('index', { title: 'Reach Me' });
 });
 
 
