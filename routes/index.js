812eced23cce436c646ff8e8ebecaa5b310d98b2
= <jishnusundar423@gmail.com>
2017-02-04 19:50:28 -0500

    Bonus checkpoint changes

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  let currentDate = new Date();
-  res.render('index', { 
-    title: 'Hi there!' ,
-  });
-});
+
 
 /* GET projects page. */
 router.get('/projects', (req, res, next) => {
-  res.render('index', { title: 'Projects' });
+  res.render('others', { title: 'Projects'});
 });
 
 /* GET services page. */
 router.get('/services', (req, res, next) => {
-  res.render('index', { title: 'Services' });
+  res.render('others', { title: 'Services' });
 });
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
-  res.render('index', { title: 'Who\'s Me?' });
+  res.render('others', { title: 'Who am I?' });
 });
 
 /* GET about page. */
 router.get('/contact', (req, res, next) => {
-  res.render('index', { title: 'Reach Me' });
+  res.render('others', { title: 'Reach Me' });
 });
 
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  let currentDate = new Date();
+  res.render('index', { 
+    title: 'Hi there!'  , mainContent: 'My nameis Jishnu and i\'ve been studying Software Engineering Technology for the last 2 years @ Centennial College. This website is my simple portfolio where most of my accomplishments and interests are posted. Check in to see!'
+  });
+});
 
 
 module.exports = router;
